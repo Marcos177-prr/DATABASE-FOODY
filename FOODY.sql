7854d91444cf4e25ce007a6950f97085dc9257fb
Marcos177-prr <0323106322@ut-tijuana.edu.mx>
2024-10-11 05:03:52 -0700

    Tablas Actualizadas

--- FOODY.sql
+++ FOODY.sql
@@ -6,16 +6,16 @@ create table fabrica(
     codigo VARCHAR (5)PRIMARY key UNIQUE,
     nombre varchar (20) not null,
     tel int null,
-    email VARCHAR (20) null,
+    email VARCHAR (20) null UNIQUE,
     dirCalle varchar (20)null,
     dirNUm int null,
     dirColonia varchar(20),
     cantEmp int not null,
-    ciudad varchar (20)
+    Foreign Key (ciudad) REFERENCES ciudad(codigo)
 )
 
 create table ciudad(
-    codigo VARCHAR(5) PRIMARY KEY UNIQUE,
+    codigo VARCHAR(5) PRIMARY KEY,
     nombre VARCHAR(20) not null UNIQUE,
 )
 
@@ -25,7 +25,7 @@ create table administradorFabrica(
     apePat VARCHAR(20) not null,
     apeMat VARCHAR(20) null,
     tel int null,
-    fabrica VARCHAR(20) not null
+    Foreign Key (Fabrica) REFERENCES Fabrica(codigo)
 )
 
 create table empleado(
@@ -34,30 +34,32 @@ create table empleado(
     apePat VARCHAR(20) not null,
     apeMat VARCHAR(20) null,
     tel int null,
-    correo VARCHAR(30) null,
-    fabrica VARCHAR(20) not null,
-    puesto VARCHAR(20) not null
+    correo VARCHAR(30) null UNIQUE,
+    Foreign Key (fabrica) REFERENCES fabrica(num)
+    Foreign Key (puesto) REFERENCES puesto(codigo)
 ) 
 
 create table puesto(
     codigo VARCHAR (5),
-    descripcion VARCHAR (20)
+    descripcion VARCHAR (20) UNIQUE
 )
 
 create table orden(
     num int PRIMARY KEY AUTO_INCREMENT,
     montoPago FLOAT not null,
     fecha date not null,
-    empleado int not null,
-    estado varchar not null
+    totalDescuento float not null,
+    Foreign Key (empleado) REFERENCES empleado(num)
+    Foreign Key (estado) REFERENCES estado(codigo)
 )
 
 create table recibo(
     num int PRIMARY KEY AUTO_INCREMENT,
     total float not null,
     fecha date not null,
-    reporte VARCHAR(30),
-    orden VARCHAR (30)
+    estadoReporte VARCHAR(20) not null,
+    Foreign Key (reporte) REFERENCES reporte(num)
+    Foreign Key (orden) REFERENCES orden(num)
 )
 
 create table reporte(
@@ -65,7 +67,8 @@ create table reporte(
     totalVenta int not null,
     fechaInicio  date not null,
     fechaFinal date not null,
-    Periodo date not null
+    Periodo date not null,
+    fechaElabo
 )
 
 create table estado(
@@ -74,7 +77,7 @@ create table estado(
 )
 
 create table categoria(
-    codigo VARCHAR(5),
+    codigo VARCHAR(5) PRIMARY KEY UNIQUE,
     nombre VARCHAR(20)
 )
 
@@ -83,8 +86,77 @@ create table platillo(
     nombre VARCHAR(20) not null,
     descripcion VARCHAR(50) not null,
     precio float not null,
-    descuentoAplicad not null,
+    porcenDescuento not null,
+    Foreign Key (categoria) REFERENCES categoria(codigo),
+    Foreign Key (menu) REFERENCES menu(codigo)
 )
 
+create table ingredientes(
+    num int PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR (20) not NULL,
+    FechCaducid date not NULL
+)
 
+create table pedido(
+    num int PRIMARY KEY AUTO_INCREMENT,
+    montoPago FLOAT not NULL,
+    fecha date not null,
+    Foreign Key (gerente_comedor) REFERENCES gerente_comedor(num),
+    Foreign Key (proveedor) REFERENCES proveedor(codigo)
+)
 
+create table proveedor(
+    codigo VARCHAR(5) PRIMARY KEY UNIQUE,
+    nombre VARCHAR(20) not NULL,
+    telefono INT null,
+    correo VARCHAR (20) NULL UNIQUE
+)
+
+create table gerente_comedor(
+    num int PRIMARY KEY AUTO_INCREMENT,
+    nombrePila VARCHAR(20) not NULL,
+    apePat VARCHAR (20) NOT NULL,
+    apeMat VARCHAR (20) null,
+    telefono INT null,
+    correo VARCHAR (20) null,
+    Foreign Key (comedor) REFERENCES comedor(num)
+)
+
+create table comedor(
+    num int PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(30) not NULL,
+    ubicacion VARCHAR(30) not NULL,
+    Foreign Key (fabrica) REFERENCES fabrica(codigo)
+)
+
+create table menu(
+    codigo VARCHAR (5) PRIMARY KEY UNIQUE,
+    nombre VARCHAR(30) NOT NULL,
+    descripcion VARCHAR (30) NOT NULL
+)
+
+create table plat_ingred(
+    cant_ingred int NOT NULL,
+    PRIMARY KEY(platillo,ingredientes),
+    Foreign Key (platillo) REFERENCES platillo(codigo),
+    Foreign Key (ingredientes) REFERENCES ingredientes(num)
+)
+
+create table ingred_ped(
+    cant_ingred int NOT NULL,
+    PRIMARY KEY(ingredientes,pedido),
+    Foreign Key (pedido) REFERENCES pedido(num),
+    Foreign Key (ingredientes) REFERENCES ingredientes(num)
+)
+
+create table com_menu(
+    PRIMARY KEY(ingredientes,pedido)
+)
+
+create table ord_plat(
+cant_platillos int NOT NULL,
+importe int NOT NULL,
+PRIMARY KEY(platillo,orden)
+Foreign Key (platillo) REFERENCES platillo(codigo)
+Foreign Key (orden) REFERENCES orden(num)
+)
